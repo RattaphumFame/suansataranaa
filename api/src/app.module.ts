bae3be96d718623b3147f05dc944afb1aa268d0a
R.famm__e <rattaphum.s@ku.th>
2026-08-31 11:46:01 +0700

    Delete api/src directory

--- api/src/app.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from "@nestjs/common";
-import { AttractionsModule } from "./attractions/attractions.module";
-import { ParksModule } from "./parks/parks.module";
-
-@Module({
-  imports: [AttractionsModule, ParksModule],
-})
-export class AppModule {}
