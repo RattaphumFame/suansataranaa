1f5ee343e7ad6fbd527bf6c73c771996242ef75b
R.famm__e <rattaphum.s@ku.th>
2026-08-31 14:24:17 +0700

    Delete api directory

--- api/src/parks/parks.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from "@nestjs/common";
-import { ParksController } from "./parks.controller";
-import { ParksService } from "./parks.service";
-
-@Module({
-  controllers: [ParksController],
-  providers: [ParksService],
-})
-export class ParksModule {}
